0ef9bdbbeff28ae8c7f3af42f1538360462bebb1
nohhayeong <starlikespirit@naver.com>
2024-06-18 17:36:38 +0900

    add user entity

---
+++ back/src/main/java/org/example/anibuddy/user/UserEntity.java
@@ -0,0 +1,26 @@
+package org.example.anibuddy.user;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String userPhone;
+
+    @Column(nullable = true)
+    private String userAddress;
+}
