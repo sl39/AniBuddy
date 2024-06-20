ffff71b4c99e577d0f446b79684671c4a5bd76de
SewoolLee <swcl309@naver.com>
2024-06-20 16:05:40 +0900

    Feat: security

---
+++ back/src/main/java/org/example/anibuddy/user/UserDto/UserSignUpRequestDto.java
@@ -0,0 +1,5 @@
+package org.example.anibuddy.user.UserDto;
+
+public class UserSignUpRequestDto {
+
+}
--- back/src/main/java/org/example/anibuddy/user/UserEntity.java
+++ back/src/main/java/org/example/anibuddy/user/UserEntity.java
@@ -1,8 +1,16 @@
 package org.example.anibuddy.user;
 
 import jakarta.persistence.*;
+import jakarta.servlet.http.PushBuilder;
+import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Builder
+@AllArgsConstructor
 public class UserEntity {
 
     @Id
@@ -23,4 +31,19 @@ public class UserEntity {
 
     @Column(nullable = true)
     private String userAddress;
+
+    private String refreshToken; // 리프레시 토큰
+
+
+
+    // 비밀번호 암호화 메소드
+    public void passwordEncode(PasswordEncoder passwordEncoder){
+        this.password = passwordEncoder.encode(this.password);
+    }
+
+    public void updateRefreshToken(String updatedRefreshToken){
+        this.refreshToken = updatedRefreshToken;
+    }
+
+
 }
