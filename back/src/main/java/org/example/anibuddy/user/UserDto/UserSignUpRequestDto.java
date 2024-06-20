7a4b41d52432bef0d8f5c0461dbef53f9ccbf2fb
SewoolLee <swcl309@naver.com>
2024-06-20 17:52:33 +0900

    feat: security/service

--- back/src/main/java/org/example/anibuddy/user/UserDto/UserSignUpRequestDto.java
+++ back/src/main/java/org/example/anibuddy/user/UserDto/UserSignUpRequestDto.java
@@ -1,5 +1,22 @@
 package org.example.anibuddy.user.UserDto;
 
-public class UserSignUpRequestDto {
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+public class UserSignUpRequestDto {
+    private String userName;
+
+    private String email;
+
+    private String password;
+
+    private String password2;
+
+    private String userPhone;
+
+    private String userAddress;
 }
