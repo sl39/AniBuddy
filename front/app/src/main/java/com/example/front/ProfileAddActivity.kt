fb17086b3f48b18656c5c73cbd991ffb03a6c072
codingnewwbie <abyss6715@gmail.com>
2024-06-28 16:21:20 +0900

    24.06.28. 16:21

--- front/app/src/main/java/com/example/front/ProfileAddActivity.kt
+++ front/app/src/main/java/com/example/front/ProfileAddActivity.kt
@@ -5,16 +5,63 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.front.databinding.ActivityMainBinding
+import com.example.front.databinding.ActivityProfileAddBinding
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class ProfileAddActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileAddBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.homeTopAppBar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.home_menu_bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener(ItemSelectedListener())
+    }
+
+    inner class ItemSelectedListener : BottomNavigationView.OnNavigationItemSelectedListener {
+        override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+            when (menuItem.itemId) {
+                R.id.home -> navigateToMainActivity(MainActivity.DESTINATION_HOME)
+                R.id.reservationList -> navigateToMainActivity(MainActivity.DESTINATION_RESERVATION_LIST)
+                R.id.follwingList -> navigateToMainActivity(MainActivity.DESTINATION_FOLLOWING_LIST)
+                R.id.chatList -> navigateToMainActivity(MainActivity.DESTINATION_CHAT_LIST)
+                R.id.profile -> navigateToMainActivity(MainActivity.DESTINATION_PROFILE)
+            }
+
+            return true
+        }
+    }
+
+    private fun navigateToMainActivity(destination: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra(MainActivity.EXTRA_DESTINATION, destination)
+        startActivity(intent)
+        finish() // ProfileAddActivity 종료
+    }
+}
+
+
+
+
+
+
+
+
+
+
+    /*
+    private lateinit var binding: ActivityProfileAddBinding
     private val fragmentManager: FragmentManager = supportFragmentManager
     private val fragmentHome = FragmentHome()
     private val fragmentReservationList = fragment_reservation_list()
@@ -22,48 +69,15 @@ class MainActivity : AppCompatActivity() {
     private val fragmentChatList = fragment_chat_list()
     private val fragmentProfile = fragment_profile()
 
-    companion object {
-        const val EXTRA_DESTINATION = "extra_destination"
-        const val DESTINATION_HOME = "home"
-        const val DESTINATION_RESERVATION_LIST = "reservation_list"
-        const val DESTINATION_FOLLOWING_LIST = "following_list"
-        const val DESTINATION_CHAT_LIST = "chat_list"
-        const val DESTINATION_PROFILE = "profile"
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityProfileAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.homeTopAppBar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        fragmentManager.beginTransaction().replace(R.id.menu_frame_layout, fragmentHome).commitAllowingStateLoss()
-
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.home_menu_bottom_navigation)
         bottomNavigationView.setOnNavigationItemSelectedListener(ItemSelectedListener())
-
-        handleIntent(intent)
-    }
-
-    private fun handleIntent(intent: Intent?) {
-        intent?.let {
-            val destination = it.getStringExtra(EXTRA_DESTINATION)
-            val transaction = fragmentManager.beginTransaction()
-
-            Log.d("MainActivity", "Handling intent for destination: $destination")
-
-            when (destination) {
-                DESTINATION_HOME -> transaction.replace(R.id.menu_frame_layout, fragmentHome)
-                DESTINATION_RESERVATION_LIST -> transaction.replace(R.id.menu_frame_layout, fragmentReservationList)
-                DESTINATION_FOLLOWING_LIST -> transaction.replace(R.id.menu_frame_layout, fragmentFollowingList)
-                DESTINATION_CHAT_LIST -> transaction.replace(R.id.menu_frame_layout, fragmentChatList)
-                DESTINATION_PROFILE -> transaction.replace(R.id.menu_frame_layout, fragmentProfile)
-                else -> transaction.replace(R.id.menu_frame_layout, fragmentHome)
-            }
-
-            transaction.commitAllowingStateLoss()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -71,19 +85,6 @@ class MainActivity : AppCompatActivity() {
         inflater.inflate(R.menu.search, menu)
 
         val menuItem = menu?.findItem(R.id.menu_search_icon)
-        val searchView = menuItem?.actionView as SearchView
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return true
-            }
-
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                if (query != null) {
-                    Log.d("Query", query)
-                }
-                return true
-            }
-        })
         return true
     }
 
@@ -115,4 +116,5 @@ class MainActivity : AppCompatActivity() {
             return true
         }
     }
-    }
+}
+*/
