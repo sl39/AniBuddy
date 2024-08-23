6ebf0551d27ddf168421fa21ff54650d07ebe0c5
SewoolLee <swcl309@naver.com>
2024-08-23 12:20:07 +0900

    fix

--- back/src/test/java/org/example/anibuddy/AnibuddyApplicationTests.java
+++ back/src/test/java/org/example/anibuddy/AnibuddyApplicationTests.java
@@ -14,41 +14,7 @@ import java.util.List;
 
 @SpringBootTest
 class AnibuddyApplicationTests {
-
-    private final StoreRepository storeRepository;
-//    private final StoreSummaryRepository storeSummaryRepository;
-
-    @Autowired
-    AnibuddyApplicationTests(StoreRepository storeRepository) {
-        this.storeRepository = storeRepository;
-//        this.storeSummaryRepository = storeSummaryRepository;
-    }
-
-
-    @Test
-    void contextLoads() {
-    }
-
 //    @Test
-//    public void transToSummary(){
-//        List<StoreEntity> storeEntityList =  storeRepository.findAll();
-//        List<StoreSummary> storeSummaryList = new ArrayList<>();
-//        for (StoreEntity storeEntity : storeEntityList) {
-//            StoreSummary storeSummary = StoreSummary.builder()
-//                    .id(storeEntity.getId())
-//                    .mapy(storeEntity.getMapy())
-//                    .mapx(storeEntity.getMapx())
-//                    .storeInfo(storeEntity.getStoreInfo())
-//                    .storeName(storeEntity.getStoreName())
-//                    .address(storeEntity.getAddress())
-//                    .roadaddress(storeEntity.getRoadaddress())
-//                    .phoneNumber(storeEntity.getPhoneNumber())
-//                    .openday(storeEntity.getOpenday())
-//                    .storeEntity(storeEntity)
-//                    .build();
-//            storeSummaryList.add(storeSummary);
-//        }
-//
-//        storeSummaryRepository.saveAll(storeSummaryList);
+//    void contextLoads() {
 //    }
 }
